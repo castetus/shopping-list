73f34520e49c3402c2de944646c1a47d6ce9f146
castetus <kboikov@mail.ru>
2022-07-26 20:02:12 +0300

    add basic styles

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,41 +1,44 @@
 <template>
   <q-layout view="hHh lpR fFf">
 
-    <q-header elevated class="bg-primary text-white">
+    <q-header elevated>
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
           </q-avatar>
-          Title
         </q-toolbar-title>
+        <q-space></q-space>
+        <q-btn outline color="grey-1" icon="add" @click="toggleListEditor"/>
       </q-toolbar>
     </q-header>
 
     <q-page-container>
       <router-view />
     </q-page-container>
-
+    <q-dialog v-model="listName">
+      <ListEditor :listName="''"/>
+    </q-dialog>
   </q-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
+import ListEditor from '../components/ListEditor.vue';
 
 export default defineComponent({
   name: 'MainLayout',
 
   components: {
-
+    ListEditor,
   },
 
   setup() {
-    const leftDrawerOpen = ref(false);
+    const listEditorOpen = ref(false);
 
     return {
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
+      toggleListEditor() {
+        listEditorOpen.value = !listEditorOpen.value;
       },
     };
   },
